abe9adaf33288be8b87bd038d41fcdb9f0313e4a
smrTofighi <mrtofxn@gmail.com>
2022-05-14 09:31:10 +0430

    create main screen

---
+++ lib/constant/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class SolidColor{
+  static const Color colorTheme = Color(0xff5d01d2);
+  static const Color bottomNavon = Color(0xff5d01d2);
+  static const Color bottomNavoff = Color(0xffffffff);
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:vira_app/constant/color.dart';
+import 'package:vira_app/gen/assets.gen.dart';
+import 'package:vira_app/main_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3)).then((value) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const MainScreen()));
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: Assets.images.logoSplash,
+                height: 160.0,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const SpinKitThreeBounce(
+                color: SolidColor.colorTheme,
+                size: 32.0,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
